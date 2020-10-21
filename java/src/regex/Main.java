a6b036616f287f1a089c863c849b31c56df22c31
Paulo Gabriel <pgj1085@gmail.com>
2020-10-21 14:56:24 -0300

    Ola Regex, literais

--- java/src/regex/Main.java
+++ java/src/regex/Main.java
@@ -5,16 +5,17 @@ import java.util.regex.*;
 public class Main {
 	
 	public static void main(String... args) {
-		String texto = "0,1,2,3,4,5,6,7,9,a,b,c,d,e,f";
 		
-		Pattern patternLetras = Pattern.compile("[a-f]");
-		Matcher matcher = patternLetras.matcher(texto);
+		String texto = "Casa bonita é a casa amarela da esquina com a Rua ACASALAR.";
+		
+		//valores literais o regex busca os literias flag G busca todas as ocorrencias (n consegui aplicar agora no JAVA)
+		String regex = "casa bonita";
+		
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(texto);
 		
 		while(matcher.find()) 
 			System.out.printf("Posicoes: %s, %s\t Valor: %s%n", matcher.start(), matcher.end(), matcher.group());
-		
-		
-		System.out.println(texto.replaceAll("[7-9]", "Achei"));
 		
 	}
 
