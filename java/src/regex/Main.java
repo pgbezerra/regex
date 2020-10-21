d8a7bb97661bcccf979860cafa24c25a8a6845bb
Paulo Gabriel <pgj1085@gmail.com>
2020-10-21 16:02:24 -0300

    Metacaractere . (ponto)

--- java/src/regex/Main.java
+++ java/src/regex/Main.java
@@ -8,35 +8,57 @@ public class Main {
 
 	public static void main(String... args) {
 
-		// . ? * + - ^ $ | [] {} () \ :
+		// . é um coringa, ou seja pega qualquer caractere, valido para uma posicao
 
-		String texto = "1,2,3,4,5,6,a.b c!d?e";
+		String texto = "1,2,3,4,5,6,7,8,9,0";
 
-		String regex = "\\.";
+		Pattern pattern = Pattern.compile("1.2");
+		Matcher matcher = pattern.matcher(texto);
 
-		System.out.println("\t\t\t\tUSANDO UM METACARACTERE EM FORMA LITERAL");
-
-		String[] textoDividido = texto.split(regex);
-
-		StringJoiner joiner = new StringJoiner(",", "[", "]");
-
-		for (String s : textoDividido)
-			joiner.add(s);
-
-		System.out.println(joiner.toString());
+		System.out.println("\t\t\t\tENCONTRANDO UM GRUPO USANDO UM PONTO");
+		while (matcher.find())
+			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
+					matcher.end()));
 
 		System.out.println();
-		System.out.println("\t\t\t\tUSANDO VARIOS METACARACTERE EM FORMA LITERAL");
+		System.out.println("\t\t\t\tNÃO ENCONTRANDO UM GRUPO USANDO DOIS PONTOS");
 
-		textoDividido = texto.split(",|\\.|\\?|!| ");
+		pattern = Pattern.compile("1..2");
+		matcher = pattern.matcher(texto);
 
-		joiner = new StringJoiner(",", "[", "]");
+		while (matcher.find())
+			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
+					matcher.end()));
 
-		for (String s : textoDividido)
-			joiner.add(s);
+		System.out.println();
+		System.out.println("\t\t\t\tENCONTRANDO UM GRUPO USANDO DOIS PONTOS");
+		pattern = Pattern.compile("1..,");
+		matcher = pattern.matcher(texto);
 
-		System.out.println(joiner.toString());
-		
+		while (matcher.find())
+			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
+					matcher.end()));
+
+		System.out.println();
+		System.out
+				.println("\t\t\t\tBUSCANDO COM PONTOS NO LUGAR, ONDE O META CARACTERE . TAMBEM VALE PARA O . LITERAL");
+
+		String notas = "8.3,7.1,8.8,10.0";
+		pattern = Pattern.compile("8..");
+		matcher = pattern.matcher(notas);
+		while (matcher.find())
+			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
+					matcher.end()));
+
+		System.out
+				.println("\t\t\t\tBUSCANDO COM PONTOS NO LUGAR, ONDE O META CARACTERE . É USADO DE FORMA LITERAL");
+
+		pattern = Pattern.compile(".\\..");
+		matcher = pattern.matcher(notas);
+		while (matcher.find())
+			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
+					matcher.end()));
+
 	}
 
 }
