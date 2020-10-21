f817bef6baea26c36f7e87643f7b4be3ecbbf57b
Paulo Gabriel <pgj1085@gmail.com>
2020-10-21 16:43:07 -0300

    Selecionando caracteres unicode

--- java/src/regex/Main.java
+++ java/src/regex/Main.java
@@ -8,25 +8,15 @@ public class Main {
 	public static void main(String... args) {
 
 
-		String texto = "Bom\ndia";
+		String texto = "aȲcʢd";
 		
-		Pattern pattern = Pattern.compile("...");
+		Pattern pattern = Pattern.compile("\\u0232|\\u02A2");
 		Matcher matcher = pattern.matcher(texto);
 		
 		while(matcher.find())
 			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(), matcher.end()));
 		
-		pattern = Pattern.compile("....");
-		matcher = pattern.matcher(texto);
-		
-		while(matcher.find())  //o ponto n encontra o \n
-			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(), matcher.end()));
-
-		pattern = Pattern.compile("....", Pattern.DOTALL);
-		matcher = pattern.matcher(texto);
-		
-		while(matcher.find()) 
-			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(), matcher.end()));
+	
 		
 
 		
