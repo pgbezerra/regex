e41761a00e859404e5b9fd075066980a9611bca1
Paulo Gabriel <pgj1085@gmail.com>
2020-10-21 22:01:33 -0300

    Selecionando caracteres unicode

--- java/src/regex/Main.java
+++ java/src/regex/Main.java
@@ -7,48 +7,20 @@ public class Main {
 
 	public static void main(String... args) {
 
-		String texto = "1,2,3,4,5,6,a.b c!d?e[f";
+		String texto = "áéíóú àèìòù âêîôû ç ãõ ü";
 
-		System.out.println("--------------------------CONJUNTOS NEGADOS ^ NO INICIO DO CONJUNTO ----------------------");
+		System.out.println("--------------------------SELECIONANDO INTERVALOS UNICODES ----------------------");
 
-		System.out.println("\n--------------------------NÃO SÃO LETRAS MINUSCULAS ----------------------");
-
-		Pattern pattern = Pattern.compile("[^a-z]");
+		Pattern pattern = Pattern.compile("[\\u00C0-\\u00FC]");
 		Matcher matcher = pattern.matcher(texto);
 
 		while (matcher.find())
 			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
 					matcher.end()));
-
-		System.out.println("\n--------------------------NÃO SAO NUMEROS ----------------------");
-
-		pattern = Pattern.compile("[^0-9]");
-		matcher = pattern.matcher(texto);
-
-		while (matcher.find())
-			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
-					matcher.end()));
-
-		System.out.println("\n--------------------------NEGANDO NUMEROS OS NUMEROS USANDO UM SHORTHAND E SIMBOLOS  ----------------------");
-
-		pattern = Pattern.compile("[^\\d!\\?\\[\s,\\.]");
-		matcher = pattern.matcher(texto);
-
-		while (matcher.find())
-			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
-					matcher.end()));
 		
-		String texto2 = "1: !\"#$%&'()*+,-./ 2: :;<=>?@";
-		
-		System.out.println("\n--------------------------NEGANDO SIMBOLOS USANDO DOIS INTERVALOS  ----------------------");
+		System.out.println(texto.replaceAll("[\\u00C0-\\u00FC]", "$0,"));
 
-		pattern = Pattern.compile("[^!-/:-@\s]");
-		matcher = pattern.matcher(texto2);
-
-		while (matcher.find())
-			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
-					matcher.end()));
-
+	
 	}
 
 }
