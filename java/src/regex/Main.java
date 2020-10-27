468f0ed78affb76898f06efcf0db4e95fdf1a494
Paulo Gabriel <pgj1085@gmail.com>
2020-10-27 14:26:31 -0300

    Desafio telefone

--- java/src/regex/Main.java
+++ java/src/regex/Main.java
@@ -7,16 +7,16 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String texto1 = "CPF dos aprovados:\n\t- 600.567.890-12\n\t- 765.998.345-23";
+		String texto1 = "Lista telefônica:\n\t- (11) 98756-1212\n\t-98765-4321\n\t-(24)5471-7896";
 
 		// quantificadores são gulosos (greedy)
 
 		Pattern pattern = null;
 		Matcher matcher = null;
 
-		System.out.println("----------------------DESAFIO CPF------------------");
+		System.out.println("----------------------DESAFIO TELEFONE------------------");
 
-		pattern = Pattern.compile("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", Pattern.CASE_INSENSITIVE);
+		pattern = Pattern.compile("\\(?\\d{0,2}\\)?\s?\\d{4,5}-\\d{4}", Pattern.CASE_INSENSITIVE);
 		matcher = pattern.matcher(texto1);
 
 		while (matcher.find())
