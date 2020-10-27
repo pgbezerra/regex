1988497384562dc546b53af7f1e413f3ca88b6e9
Paulo Gabriel <pgj1085@gmail.com>
2020-10-27 13:52:19 -0300

    Guloso e nao guloso

--- java/src/regex/Main.java
+++ java/src/regex/Main.java
@@ -7,54 +7,67 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String texto1 = "O João recebeu 120 milhões apostando 6 9 21 23 45 46";
+		String texto1 = "<div>Conteudo 01</div><div>Conteudo 02</div>";
 
-		// Para definir um quantificador vc utiliza a {}
+		// quantificadores são gulosos (greedy)
 
 		Pattern pattern = null;
 		Matcher matcher = null;
 
-		System.out.println("----------------------QUANTIFICADORES {}------------------");
+		System.out.println("----------------------QUANTIFICADORES GULOSOS------------------");
 
-		pattern = Pattern.compile("\\d{1,2}", Pattern.CASE_INSENSITIVE);
+		pattern = Pattern.compile("<div>.+<\\/div>", Pattern.CASE_INSENSITIVE);
 		matcher = pattern.matcher(texto1);
 
-		System.out.println("\n----------------------NUMEROS ENTRE 1 e 2 DIGITOS------------------");
+		System.out.println("\n----------------------ENCONTRANDO AS DIVS GULOSO COM +------------------");
 
 		while (matcher.find())
 			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
 					matcher.end()));
 
-		System.out.println("\n----------------------NUMEROS APENAS COM 2 DIGITOS------------------");
+		System.out.println("\n----------------------ENCONTRANDO AS DIVS GULOSO COM *------------------");
 
-		pattern = Pattern.compile("[0-9]{2}", Pattern.CASE_INSENSITIVE);
-		matcher = pattern.matcher(texto1);
-
-		while (matcher.find())
-			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
-					matcher.end()));
-
-		System.out.println("\n----------------------NUMEROS COM 1 OU MAIS DIGITOS------------------");
-
-		pattern = Pattern.compile("[0-9]{1,}", Pattern.CASE_INSENSITIVE);
+		pattern = Pattern.compile("<div>.*<\\/div>", Pattern.CASE_INSENSITIVE);
 		matcher = pattern.matcher(texto1);
 
 		while (matcher.find())
 			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
 					matcher.end()));
 
-		System.out.println("\n----------------------CARACTERES COM 7 DIGITOS------------------");
+		System.out.println("\n----------------------ENCONTRANDO AS DIVS GULOSO COM {}------------------");
 
-		pattern = Pattern.compile("\\w{7}", Pattern.CASE_INSENSITIVE);
+		pattern = Pattern.compile("<div>.{0,100}<\\/div>", Pattern.CASE_INSENSITIVE);
 		matcher = pattern.matcher(texto1);
 
 		while (matcher.find())
 			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
 					matcher.end()));
 
-		System.out.println("\n----------------------CARACTERES COM 7 DIGITOS OU MAIS USANDO UNICODE------------------");
 
-		pattern = Pattern.compile("[\\wõ]{7,}", Pattern.CASE_INSENSITIVE);
+
+		System.out.println("\n\n----------------------QUANTIFICADORES NÃO GULOSOS------------------");
+
+		pattern = Pattern.compile("<div>.+?<\\/div>", Pattern.CASE_INSENSITIVE);
+		matcher = pattern.matcher(texto1);
+
+		System.out.println("\n----------------------ENCONTRANDO AS DIVS NÃO GULOSO COM +------------------");
+
+		while (matcher.find())
+			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
+					matcher.end()));
+
+		System.out.println("\n----------------------ENCONTRANDO AS DIVS NÃO GULOSO COM *------------------");
+
+		pattern = Pattern.compile("<div>.*?<\\/div>", Pattern.CASE_INSENSITIVE);
+		matcher = pattern.matcher(texto1);
+
+		while (matcher.find())
+			System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(),
+					matcher.end()));
+
+		System.out.println("\n----------------------ENCONTRANDO AS DIVS NÃO GULOSO COM {}------------------");
+
+		pattern = Pattern.compile("<div>.{0,100}?<\\/div>", Pattern.CASE_INSENSITIVE);
 		matcher = pattern.matcher(texto1);
 
 		while (matcher.find())
