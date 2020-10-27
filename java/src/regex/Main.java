607921851c82379a7b74955ca2edbb1720b0f6d6
Paulo Gabriel <pgj1085@gmail.com>
2020-10-27 17:12:40 -0300

    Cuidados com grupos

--- java/src/regex/Main.java
+++ java/src/regex/Main.java
@@ -7,22 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
         
-    	String texto1 = "supermercado hipermercado minimercado mercado";
-    	String texto2 = "Lentamente é mente muito lenta";
+    	String texto1 = "Pedrinho (filho do Pedro Silva) é doutor do ABC!";
     	
     	Pattern pattern = null;
     	Matcher matcher = null;
     	
-    	System.out.println("\n--------------------GRUPOS COM OU--------------------");
-    	pattern = Pattern.compile("(super|hiper|mini)?mercado"); //valor armazenado a partir do grupo é referenciado com \1, \2... O valor do grupo nem sempre é armazeanado
+    	System.out.println("\n--------------------CUIDADO 1--------------------");
+    	pattern = Pattern.compile("[(abc)]", Pattern.CASE_INSENSITIVE); 
     	matcher = pattern.matcher(texto1);
     	
     	while(matcher.find())
     		System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(), matcher.end()));
     	
-    	System.out.println("\n--------------------GRUPOS ANINHADOS--------------------");
-    	pattern = Pattern.compile("((hi|su)per|mini)?mercado", Pattern.CASE_INSENSITIVE);
+    	System.out.println("\n--------------------DENTRO DE UM CONJUNTO UM GRUPO NÃO EXISTE--------------------");
+    	
+    	System.out.println("\n--------------------CONJUNTO DENTRO DE UM GRUPO--------------------");
+    	pattern = Pattern.compile("([abc])", Pattern.CASE_INSENSITIVE);
     	matcher = pattern.matcher(texto1);
+    	
+    	while(matcher.find())
+    		System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(), matcher.end()));
     	
     }
     
