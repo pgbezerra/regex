b97e65d9f086a2c6c47b83fc24741f52b0298408
Paulo Gabriel <pgj1085@gmail.com>
2020-10-27 16:18:26 -0300

    Grupos & Retovisores

--- java/src/regex/Main.java
+++ java/src/regex/Main.java
@@ -7,25 +7,45 @@ public class Main {
 
     public static void main(String[] args) {
         
-    	String texto1 = "O José Simão é muito engraçado... hehehehehe";
-    	String texto2 = "http://www.site.info www.escola.ninja.br google.com.br";
+    	String texto1 = "<b>Destaque</b><strong>Forte</strong><div>Conteudo</div>";
+    	String texto2 = "Lentamente é mente muito lenta";
     	
     	Pattern pattern = null;
     	Matcher matcher = null;
     	
-    	System.out.println("\n--------------------GRUPOS--------------------");
-    	pattern = Pattern.compile("(he)+");
+    	System.out.println("\n--------------------GRUPOS & RETROVISORES--------------------");
+    	pattern = Pattern.compile("<(\\w+)>.*</\\1>"); //valor armazenado a partir do grupo é referenciado com \1, \2... O valor do grupo nem sempre é armazeanado
     	matcher = pattern.matcher(texto1);
     	
     	while(matcher.find())
     		System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(), matcher.end()));
     	
-    	System.out.println("\n--------------------URL--------------------");
-    	pattern = Pattern.compile("(http:\\/\\/)?(www\\.)?\\w+.\\w{2,}(\\.\\w{2})?");
+    	System.out.println("\n--------------------MAIS EXEMPLOS--------------------");
+    	pattern = Pattern.compile("(lenta)(mente).*\\2.*\\1", Pattern.CASE_INSENSITIVE);
     	matcher = pattern.matcher(texto2);
     	
     	while(matcher.find())
     		System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(), matcher.end()));
+    	
+    	System.out.println("\n--------------------MAIS EXEMPLOS SEM ARMAZENAMENTO--------------------");
+    	
+    	pattern = Pattern.compile("(?:lenta)(mente).*\\1", Pattern.CASE_INSENSITIVE); //o ?: faz com que o grupo não seja armazenado
+    	matcher = pattern.matcher(texto2);
+    	
+    	while(matcher.find())
+    		System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(), matcher.end()));
+    	
+    	
+    	System.out.println("\n--------------------EXEMPLO COM GRUPO OPCIONAL--------------------");
+    	pattern = Pattern.compile("(lenta)(mente)?", Pattern.CASE_INSENSITIVE); //valor armazenado a partir do grupo é referenciado com \1, \2... O valor do grupo nem sempre é armazeanado
+    	matcher = pattern.matcher(texto2);
+    	
+    	while(matcher.find())
+    		System.out.println(String.format("Encontrado %s na posicao %s ate %s", matcher.group(), matcher.start(), matcher.end()));
+    	
+    	System.out.println("\n--------------------EXEMPLO COM REPLACE--------------------\n\n");
+    	System.out.println(texto2.toLowerCase().replaceAll("(lenta)(mente)", "$2"));
+    	
     	
     }
     
